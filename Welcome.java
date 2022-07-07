2707f87d9a7a5c2b3c569337040c9788679ad084
Sama872 <107773332+Sama872@users.noreply.github.com>
2022-07-06 19:32:59 -0700

    Add files via upload

---
+++ Welcome.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Welcome;
+
+/**
+ *
+ * @author hp
+ */
+public class Welcome {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        class circle{
+            int diameter;
+            circle c = new circle();
+    }
+        // TODO code application logic here
+    }
+    
+}
